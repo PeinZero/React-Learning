8a7e9d21443b0862f287e66f30281b095de880aa
PeinZero <mahadzx@gmail.com>
2021-01-24 21:44:46 +0500

    basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  state = {
+    persons: [
+                { name: 'mahad', age: 21 },
+                { name: 'ammar', age: 22 },
+                { name: 'abdullah', age: 23 }
+    ],
+    human: true
+  }
+
+  switchNameHandler = (newName) =>{
+    this.setState({persons: [
+      { name: newName, age: 22 },
+      { name: 'ammar', age: 22 },
+      { name: 'abdullah', age: 23 }
+    ]})
+  }
+
+  changedNameHandler = (event) =>{
+    this.setState({persons: [
+      { name:  'mahad', age: 22 },
+      { name: 'ammar', age: 22 },
+      { name: event.target.value, age: 23 } 
+    ]})
+  }
+
+  render(){
+    return(
+      <div className = 'App'>
+        <h1>Hey, man</h1>
+        
+        {/* First Method of sending an argument in function switchNameHandler i.e 'Peinzero' using bind() */}
+        {/* This is better practise than Second Method */}
+        <button onClick = {this.switchNameHandler.bind(this,'Peinzero')}> Switch Name</button>
+        
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}> I'm learning React</Person>  
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> I'm learning Vue</Person>
+        
+        {/* sending switchNameHandler as an argument to person Component in Person.js  */}
+        {/* Second Method of sending an argument in function switchNameHandler i.e 'Maddy' using an Anonymous function */}
+        <Person 
+          name = {this.state.persons[2].name}
+          age = {this.state.persons[2].age}
+          click = {() => this.switchNameHandler('Maddy')}
+          changed = {this.changedNameHandler}
+        />  
+
+      </div>
+    )
+  }
 }
 
 export default App;
